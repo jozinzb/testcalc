c7f1afaf78b361a9c7ee59782ebd60cb6702e561
Pavel Farar <Pavel@my.org>
2020-08-07 15:04:10 +0200

    Add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+Testing calculator library.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtrack(first_term, second_term):
+    return first_term - second_term
