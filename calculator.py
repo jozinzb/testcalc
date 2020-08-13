2f4cf6072253e26ebf6c94517cafdcb52bea77ed
Pavel Farar <Pavel@my.org>
2020-08-13 11:25:25 +0200

    Add function multiply() with just one empty line

--- calculator.py
+++ calculator.py
@@ -11,3 +11,7 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     """Subtrack two values"""
     return first_term - second_term
+
+def multiply(first_term, second_term):
+    """Multiply two values"""
+    return first_term * second_term
